0c99a54ec621fa1690e03744e7d8de4c3f0ae6d1
sherri010 <aminshahidi.sh@gmail.com>
2016-12-31 13:16:46 -0800

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,30 @@
 Rails.application.routes.draw do
   root to: 'cities#index'
 
-  get '/cities', to: 'cities#index', as: 'cities'
-  get '/cities/new', to: 'cities#new' , as: 'new_city'
-  get '/cities/:id', to: 'cities#show', as: 'city'
-  post '/cities', to:'cities#create', as:'add_city'
-  patch '/citie/:id', to: 'cities#update', as:'update_city'
-  delete '/cities/:id', to: 'cities#destroy', as:'delete_city'
-
+  # get '/cities', to: 'cities#index', as: 'cities'
+  # get '/cities/new', to: 'cities#new' , as: 'new_city'
+  # get '/cities/:id', to: 'cities#show', as: 'city'
+  # post '/cities', to:'cities#create', as:'add_city'
+  # patch '/citie/:id', to: 'cities#update', as:'update_city'
+  # delete '/cities/:id', to: 'cities#destroy', as:'delete_city'
+  #
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
-
-  # get '/users/:user_id/posts',     to: 'post_users#index', as: 'user_posts'
-  # post '/posts/:post_id/users', to: 'post_users#create', as: 'post_users'
-
-  patch '/users/:id', to: 'users#update', as: 'update_user'
-
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'show_post'
-
-  delete '/cities/:city_id/posts/:id', to: 'posts#destroy', as: 'delete_post'
-
-  patch '/cities/:city_id/posts/:id', to: 'posts#update', as: 'update_post'
-
-  patch '/cities/:city_id/posts/:post_id/comments/:id', to: 'comments#update', as: 'update_comment'
-  delete '/cities/:city_id/posts/:post_id/comments/:id', to: 'comments#destroy', as: 'delete_comment'
+  #
+  # # get '/users/:user_id/posts',     to: 'post_users#index', as: 'user_posts'
+  # # post '/posts/:post_id/users', to: 'post_users#create', as: 'post_users'
+  #
+  # patch '/users/:id', to: 'users#update', as: 'update_user'
+  #
+   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'show_post'
+  #
+  # delete '/cities/:city_id/posts/:id', to: 'posts#destroy', as: 'delete_post'
+  #
+  # patch '/cities/:city_id/posts/:id', to: 'posts#update', as: 'update_post'
+  #
+  # patch '/cities/:city_id/posts/:post_id/comments/:id', to: 'comments#update', as: 'update_comment'
+  # delete '/cities/:city_id/posts/:post_id/comments/:id', to: 'comments#destroy', as: 'delete_comment'
 
   resources :users, except: :index
 
